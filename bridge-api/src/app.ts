c890789a271a143cc5a3d2e7c1c627f34e7edf50
Igor <igor@trustmachines.co>
2023-03-23 16:40:40 -0500

    update

--- bridge-api/src/app.ts
+++ bridge-api/src/app.ts
@@ -6,6 +6,8 @@ import Router from "./routes/index.js";
 import { serve, setup } from 'swagger-ui-express';
 import { sbtcEventJob } from './controllers/JobScheduler.js';
 import cors from "cors";
+import https from "https";
+import fs from "fs";
 
 const app = express();
 app.use(express.json());
@@ -26,11 +28,22 @@ app.use(
 
 app.use(Router);
 
-app.listen(port, () => {
-  dumpConfig();
-  console.log(`Express is listening at http://localhost:${port} \nwallet: ${sbtcContractId}`);
-  return;
-});
+// app.listen(port, () => {
+//   dumpConfig();
+//   console.log(`Express is listening at http://localhost:${port} \nwallet: ${sbtcContractId}`);
+//   return;
+// });
+
+https
+.createServer(
+    // Provide the private and public key to the server by reading each
+    // file's content with the readFileSync() method.
+    {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+    },
+    app
+)
 
 sbtcEventJob.start();
 console.log(`Running on ${host}:${port}\n\n`);
