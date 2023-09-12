a656c175a486c336167a6a0b46fb0580c112809e
Mike Cohen <mjoecohen@gmail.com>
2023-09-12 20:59:59 +0100

    Middleware to pull the stacks signature and compare with the stacks address. Also compares per request to update/save transactions.

--- sbtc-bridge-api/src/lib/stacks_helper.ts
+++ sbtc-bridge-api/src/lib/stacks_helper.ts
@@ -1,6 +1,39 @@
 
+import { hashMessage } from '@stacks/encryption';
 import { c32address, c32addressDecode } from 'c32check';
+import { hex } from '@scure/base';
+import util from 'util'
+import { publicKeyFromSignatureRsv, type MessageSignature, StacksMessageType, publicKeyFromSignatureVrs } from '@stacks/transactions';
+import { getStacksAddressFromPubkey } from 'sbtc-bridge-lib/dist/payload_utils';
+import { getConfig } from './config';
+const authMessage = 'Please sign this message to complete authentication'
 
+export function isUpdateAllowed(req, stacksAddressFromRequest:string) {
+	const stacksAddressSig = stacksAddressFromSignatureHeader(req.headers.authorization)
+	console.log('isUpdateAllowed: stacksAddressFromRequest' + stacksAddressFromRequest)
+	console.log('isUpdateAllowed: stacksAddressSig' + stacksAddressSig)
+	return stacksAddressSig === stacksAddressFromRequest;
+}
+
+function stacksAddressFromSignatureHeader(authorization:any) {
+	const network = getConfig().network
+	const decoded = JSON.parse(authorization)
+	const msgHash = hashMessage(authMessage);
+	const pubkey = publicKeyFromSignatureVrs(hex.encode(msgHash), { data: decoded.signature, type: StacksMessageType.MessageSignature })
+	const stacks = getStacksAddressFromPubkey(hex.decode(pubkey))
+	if (network === 'testnet') return stacks.tp2pkh
+	else return stacks.mp2pkh
+}
+
+export function authorised(authorization:any) {
+	if (!authorization) return false;
+	const decoded = JSON.parse(authorization)
+	const stacksAddressSig = stacksAddressFromSignatureHeader(authorization)
+	console.log('stacksAddressSig: ' + stacksAddressSig)
+	console.log('decoded.stxAddress: ' + decoded.stxAddress)
+	return stacksAddressSig === decoded.stxAddress;
+}
+  
 export function decodeStacksAddress(stxAddress:string) {
 	if (!stxAddress) throw new Error('Needs a stacks address');
 	const decoded = c32addressDecode(stxAddress)
